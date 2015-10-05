c1a7f47418d9c9b4a83428d7fcc04297f2678fcb
vladzaets <vladzaets@gmail.com>
2015-10-05 11:37:46 +0700

    Fix

--- lib/sms_area_api/api.rb
+++ lib/sms_area_api/api.rb
@@ -12,7 +12,17 @@ module SmsAreaApi
         api_key: api_key
       }.merge(params)
 
-      RestClient.post 'http://sms-area.org/stubs/handler_api.php', request_params
+      response = RestClient.post 'http://sms-area.org/stubs/handler_api.php', request_params
+
+      if response =~ /ACCESS_NUMBER:/
+        data = response.split(':')
+        return {
+          id: data[1],
+          access_number: data[2]
+        }
+      else
+        return response
+      end
     end
 
     def method_missing(method, **args)  
